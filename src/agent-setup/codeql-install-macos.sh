dbfc35434c4b91873056676be5d50197e1ac5c11
Nick Couraud <ncouraud@users.noreply.github.com>
2023-05-08 21:33:04 -0400

    add CodeQL Setup Scripts for Self-Hosted Agents (#3)
    * add CodeQL Setup Scripts
    
    * update for PSScriptAnalyzer feedback

---
+++ src/agent-setup/codeql-install-macos.sh
@@ -0,0 +1,85 @@
+
+#!/bin/bash -e -o pipefail
+################################################################################
+##  File:  codeql-install-macos.sh
+##  Desc:  Install the CodeQL CLI Bundles to the toolcache. 
+##         Borrowed from https://github.com/actions/runner-images/
+################################################################################
+
+
+download_with_retries() {
+# Due to restrictions of bash functions, positional arguments are used here.
+# In case if you using latest argument NAME, you should also set value to all previous parameters.
+# Example: download_with_retries $ANDROID_SDK_URL "." "android_sdk.zip"
+    local URL="$1"
+    local DEST="${2:-.}"
+    local NAME="${3:-${URL##*/}}"
+    local COMPRESSED="$4"
+
+    if [[ $COMPRESSED == "compressed" ]]; then
+        local COMMAND="curl $URL -4 -sL --compressed -o '$DEST/$NAME' -w '%{http_code}'"
+    else
+        local COMMAND="curl $URL -4 -sL -o '$DEST/$NAME' -w '%{http_code}'"
+    fi
+
+    echo "Downloading '$URL' to '${DEST}/${NAME}'..."
+    retries=20
+    interval=30
+    while [ $retries -gt 0 ]; do
+        ((retries--))
+        # Temporary disable exit on error to retry on non-zero exit code
+        set +e
+        http_code=$(eval $COMMAND)
+        exit_code=$?
+        if [ $http_code -eq 200 ] && [ $exit_code -eq 0 ]; then
+            echo "Download completed"
+            return 0
+        else
+            echo "Error — Either HTTP response code for '$URL' is wrong - '$http_code' or exit code is not 0 - '$exit_code'. Waiting $interval seconds before the next attempt, $retries attempts left"
+            sleep 30
+        fi
+        # Enable exit on error back
+        set -e
+    done
+
+    echo "Could not download $URL"
+    return 1
+}
+
+# Retrieve the name of the CodeQL bundle preferred by the Action (in the format codeql-bundle-YYYYMMDD).
+base_url="$(curl -sSL https://raw.githubusercontent.com/github/codeql-action/v2/src/defaults.json)"
+codeql_tag_name="$(echo "$base_url" | jq -r '.bundleVersion')"
+codeql_cli_version="$(echo "$base_url" | jq -r '.cliVersion')"
+prior_codeql_tag_name="$(echo "$base_url" | jq -r '.priorBundleVersion')"
+prior_codeql_cli_version="$(echo "$base_url" | jq -r '.priorCliVersion')"
+
+# Convert the tag names to bundles with a version number (x.y.z-YYYYMMDD).
+codeql_bundle_version="${codeql_cli_version}-${codeql_tag_name##*-}"
+prior_codeql_bundle_version="${prior_codeql_cli_version}-${prior_codeql_tag_name##*-}"
+
+# Download and name both CodeQL bundles.
+codeql_bundle_versions=("${codeql_bundle_version}" "${prior_codeql_bundle_version}")
+codeql_tag_names=("${codeql_tag_name}" "${prior_codeql_tag_name}")
+
+for index in "${!codeql_bundle_versions[@]}"; do
+  bundle_version="${codeql_bundle_versions[$index]}"
+  bundle_tag_name="${codeql_tag_names[$index]}"
+  
+  echo "Downloading CodeQL bundle $bundle_version..."
+    download_with_retries "https://github.com/github/codeql-action/releases/download/$bundle_tag_name/codeql-bundle.tar.gz" "/tmp" "codeql-bundle.tar.gz"
+    codeql_archive="/tmp/codeql-bundle.tar.gz"
+
+    codeql_toolcache_path="$AGENT_TOOLSDIRECTORY/CodeQL/$bundle_version/x64"
+    mkdir -p "$codeql_toolcache_path"
+
+    echo "Unpacking the downloaded CodeQL bundle archive..."
+    tar -xzf "$codeql_archive" -C "$codeql_toolcache_path"
+
+    # We only pin the latest version in the toolcache, to support overriding the CodeQL version specified in defaults.json on GitHub Enterprise.
+    if [[ "$bundle_version" == "$codeql_bundle_version" ]]; then
+        touch "$codeql_toolcache_path/pinned-version"
+    fi
+
+    # Touch a file to indicate to the toolcache that setting up CodeQL is complete.
+    touch "$codeql_toolcache_path.complete"
+done
---
+++ src/agent-setup/codeql-install-windows.ps1
@@ -0,0 +1,120 @@
+################################################################################
+##  File:  CodeQL-Install-Windows.ps1
+##  Desc:  Install the CodeQL CLI Bundles.
+##         Borrowed from: https://github.com/actions/runner-images
+##         Requires 7Zip to extract tar/gzip archives
+################################################################################
+
+function Get-DownloadWithRetry {
+    Param
+    (
+        [Parameter(Mandatory)]
+        [string] $Url,
+        [string] $Name,
+        [string] $DownloadPath = "${env:Temp}",
+        [int] $Retries = 20
+    )
+
+    if ([String]::IsNullOrEmpty($Name)) {
+        $Name = [IO.Path]::GetFileName($Url)
+    }
+
+    $filePath = Join-Path -Path $DownloadPath -ChildPath $Name
+    $downloadStartTime = Get-Date
+
+    # Default retry logic for the package.
+    while ($Retries -gt 0)
+    {
+        try
+        {
+            $downloadAttemptStartTime = Get-Date
+            Write-Output "Downloading package from: $Url to path $filePath ."
+            (New-Object System.Net.WebClient).DownloadFile($Url, $filePath)
+            break
+        }
+        catch
+        {
+            $failTime = [math]::Round(($(Get-Date) - $downloadStartTime).TotalSeconds, 2)
+            $attemptTime = [math]::Round(($(Get-Date) - $downloadAttemptStartTime).TotalSeconds, 2)
+            Write-Output "There is an error encounterd after $attemptTime seconds during package downloading:`n $_"
+            $Retries--
+
+            if ($Retries -eq 0)
+            {
+                Write-Output "File can't be downloaded. Please try later or check that file exists by url: $Url"
+                Write-Output "Total time elapsed $failTime"
+                exit 1
+            }
+
+            Write-Output "Waiting 30 seconds before retrying. Retries left: $Retries"
+            Start-Sleep -Seconds 30
+        }
+    }
+
+    $downloadCompleteTime = [math]::Round(($(Get-Date) - $downloadStartTime).TotalSeconds, 2)
+    Write-Output "Package downloaded successfully in $downloadCompleteTime seconds"
+    return $filePath
+}
+
+function Expand-7Zip {
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]$Path,
+        [Parameter(Mandatory=$true)]
+        [string]$DestinationPath
+    )
+
+    Write-Output "Expand archive '$PATH' to '$DestinationPath' directory"
+    7z.exe x "$Path" -o"$DestinationPath" -y | Out-Null
+
+    if ($LASTEXITCODE -ne 0)
+    {
+        Write-Output "There is an error during expanding '$Path' to '$DestinationPath' directory"
+        exit 1
+    }
+}
+
+# Retrieve the name of the CodeQL bundle preferred by the Action (in the format codeql-bundle-YYYYMMDD).
+$Defaults = (Invoke-RestMethod "https://raw.githubusercontent.com/github/codeql-action/v2/src/defaults.json")
+$CodeQLTagName = $Defaults.bundleVersion
+$CodeQLCliVersion = $Defaults.cliVersion
+$PriorCodeQLTagName = $Defaults.priorBundleVersion
+$PriorCodeQLCliVersion = $Defaults.priorCliVersion
+
+# Convert the tag names to bundles with a version number (x.y.z-YYYYMMDD).
+$CodeQLBundleVersion = $CodeQLCliVersion + "-" + $CodeQLTagName.split("-")[-1]
+$PriorCodeQLBundleVersion = $PriorCodeQLCliVersion + "-" + $PriorCodeQLTagName.split("-")[-1]
+
+$Bundles = @(
+    [PSCustomObject]@{
+        TagName=$CodeQLTagName;
+        BundleVersion=$CodeQLBundleVersion;
+    },
+    [PSCustomObject]@{
+        TagName=$PriorCodeQLTagName;
+        BundleVersion=$PriorCodeQLBundleVersion;
+    }
+)
+
+foreach ($Bundle in $Bundles) {
+    Write-Output "Downloading CodeQL bundle $($Bundle.BundleVersion)..."
+    $CodeQLBundlePath = Get-DownloadWithRetry -Url "https://github.com/github/codeql-action/releases/download/$($Bundle.TagName)/codeql-bundle.tar.gz" -Name "codeql-bundle.tar.gz"
+    $DownloadDirectoryPath = (Get-Item $CodeQLBundlePath).Directory.FullName
+
+    $CodeQLToolcachePath = Join-Path $Env:AGENT_TOOLSDIRECTORY -ChildPath "CodeQL" | Join-Path -ChildPath $Bundle.BundleVersion | Join-Path -ChildPath "x64"
+    New-Item -Path $CodeQLToolcachePath -ItemType Directory -Force | Out-Null
+
+    Write-Output "Unpacking the downloaded CodeQL bundle archive..."
+    Expand-7Zip -Path $CodeQLBundlePath -DestinationPath $DownloadDirectoryPath
+    $UnGzipedCodeQLBundlePath = Join-Path $DownloadDirectoryPath "codeql-bundle.tar"
+    Expand-7Zip -Path $UnGzipedCodeQLBundlePath -DestinationPath $CodeQLToolcachePath
+
+    # We only pin the latest version in the toolcache, to support overriding the CodeQL version specified in defaults.json on GitHub Enterprise.
+    if ($Bundle.BundleVersion -eq $CodeQLBundleVersion) {
+        New-Item -ItemType file (Join-Path $CodeQLToolcachePath -ChildPath "pinned-version")
+    }
+
+    # Touch a file to indicate to the toolcache that setting up CodeQL is complete.
+    New-Item -ItemType file "$CodeQLToolcachePath.complete"
+}
