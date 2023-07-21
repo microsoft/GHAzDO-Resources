3d2a01ed7c4d83bd0eaca0f6422d545fa91e25c3
Nicolas Couraud <nicour@microsoft.com>
2023-07-21 09:55:44 -0400

    update based on PsScriptAnalyzer Feedback

--- src/gating/gating.ps1
+++ src/gating/gating.ps1
@@ -1,6 +1,6 @@
 $pass = ${env:MAPPED_ADO_PAT} #$env:MAPPED_ADO_PAT = "TODO-ADVSEC-SCOPED-PAT-HERE"
-$orgUri = ${env:SYSTEM_COLLECTIONURI} #$(System.CollectionUri) #$env:SYSTEM_COLLECTIONURI = "https://dev.azure.com/TODO-YOUR-ORG-HERE/" 
-$orgName = $orgUri -replace "^https://dev.azure.com/|/$" 
+$orgUri = ${env:SYSTEM_COLLECTIONURI} #$(System.CollectionUri) #$env:SYSTEM_COLLECTIONURI = "https://dev.azure.com/TODO-YOUR-ORG-HERE/"
+$orgName = $orgUri -replace "^https://dev.azure.com/|/$"
 $project = ${env:SYSTEM_TEAMPROJECT} #$(System.TeamProject) #$env:SYSTEM_TEAMPROJECT = "TODO-YOUR-PROJECT-NAME-HERE"
 $repositoryId = ${env:BUILD_REPOSITORY_ID} #$(Build.Repository.ID) #env:BUILD_REPOSITORY_ID= "TODO-YOUR-REPO-GUID-HERE"
 $pair = ":${pass}"
@@ -10,18 +10,18 @@ $basicAuthValue = "Basic $base64"
 $headers = @{ Authorization = $basicAuthValue }
 $url = "https://advsec.dev.azure.com/{0}/{1}/_apis/AdvancedSecurity/Repositories/{2}/alerts?useDatabaseProvider=true" -f $orgName, $project, $repositoryId
 
-$alerts = Invoke-WebRequest -Uri $url -Headers $headers -Method Get 
+$alerts = Invoke-WebRequest -Uri $url -Headers $headers -Method Get
 if ($alerts.StatusCode -ne 200) {
-    Write-Host "##vso[task.logissue type=error] Error getting alerts from Azure DevOps Advanced Security:", $alerts.StatusCode, $alerts.StatusDescription
+    Write-Information "##vso[task.logissue type=error] Error getting alerts from Azure DevOps Advanced Security:", $alerts.StatusCode, $alerts.StatusDescription
     exit 1
 }
-$parsedAlerts = $alerts.content | ConvertFrom-Json 
+$parsedAlerts = $alerts.content | ConvertFrom-Json
 
 #Region ADD TEMPORARY SUPPORT FOR Non-Database Provider for Dependency Alerts in Public Preview, this will migrate to Database Provider in the near future
 $urlTemp = "https://advsec.dev.azure.com/{0}/{1}/_apis/AdvancedSecurity/Repositories/{2}/alerts?useDatabaseProvider=false" -f $orgName, $project, $repositoryId
-$alertsTemp = Invoke-WebRequest -Uri $urlTemp -Headers $headers -Method Get 
+$alertsTemp = Invoke-WebRequest -Uri $urlTemp -Headers $headers -Method Get
 if ($alertsTemp.StatusCode -ne 200) {
-    Write-Host "##vso[task.logissue type=error] Error getting alerts from Azure DevOps Advanced Security:", $alertsTemp.StatusCode, $alertsTemp.StatusDescription
+    Write-Information "##vso[task.logissue type=error] Error getting alerts from Azure DevOps Advanced Security:", $alertsTemp.StatusCode, $alertsTemp.StatusDescription
     exit 1
 }
 $parsedAlerts.value += $($alertsTemp.content | ConvertFrom-Json).value | Where-Object { $_.alertType -eq "dependency" }
@@ -41,7 +41,7 @@ $failingAlerts = foreach ($alert in $parsedAlerts.value) {
             -and $alert.state -in $states `
             -and $alert.firstSeen -as [DateTime] -lt (Get-Date).ToUniversalTime().AddDays(-$slaDays) `
             -and $alert.alertType -in $alertTypes) {
-        @{ 
+        @{
             "Alert Title"  = $alert.title
             "Alert Id"     = $alert.alertId
             "Severity"     = $alert.severity
@@ -56,11 +56,11 @@ $failingAlerts = foreach ($alert in $parsedAlerts.value) {
 
 if ($failingAlerts.Count -gt 0) {
     $errorText = "##vso[task.logissue type=error] Found {0} failing alerts out of SLA policy:" -f $failingAlerts.Count
-    Write-Host $errorText
-    $failingAlerts | Format-Table -AutoSize | Out-String | Write-Host
+    Write-Information $errorText
+    $failingAlerts | Format-Table -AutoSize | Out-String | Write-Information
     exit 1
 }
 else {
-    Write-Host "##vso[task.complete result=Succeeded;]DONE"
-    exit 0 
+    Write-Information "##vso[task.complete result=Succeeded;]DONE"
+    exit 0
 }
