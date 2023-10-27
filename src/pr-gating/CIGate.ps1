c99f69b3d735a4173d7271cc9575467a10d44966
Jonas Helin <106392052+Jfhelin@users.noreply.github.com>
2023-10-27 16:33:22 +0200

    Update CIGate.ps1

--- src/pr-gating/CIGate.ps1
+++ src/pr-gating/CIGate.ps1
@@ -13,16 +13,14 @@ $base64 = [System.Convert]::ToBase64String($bytes)
 $basicAuthValue = "Basic $base64"
 $headers = @{ Authorization = $basicAuthValue }
 
-$alertType = "" # code,dependency,secret - If provided, only return alerts of this type. Otherwise, return alerts of all types.
+$urlTargetAlerts = "https://advsec.dev.azure.com/{0}/{1}/_apis/Alert/repositories/{2}/Alerts?top=500&orderBy=lastSeen&criteria.alertType=3&criteria.ref={3}&criteria.states=1" -f $orgName, $project, $repositoryId, $prTargetBranch
+$urlSourceAlerts = "https://advsec.dev.azure.com/{0}/{1}/_apis/Alert/repositories/{2}/Alerts?top=500&orderBy=lastSeen&criteria.alertType=3&criteria.ref={3}&criteria.states=1" -f $orgName, $project, $repositoryId, $prSourceBranch
 
-$urlTargetAlerts = "https://advsec.dev.azure.com/{0}/{1}/_apis/Alert/Repositories/{2}/Alerts?top=5000&orderBy=lastSeen&criteria.alertType={4}&criteria.branchName={3}&criteria.onlyDefaultBranchAlerts=true&useDatabaseProvider=true" -f $orgName, $project, $repositoryId, $prTargetBranch, $alertType
-$urlSourceAlerts = "https://advsec.dev.azure.com/{0}/{1}/_apis/Alert/repositories/{2}/Alerts?top=5000&orderBy=lastSeen&criteria.alertType={4}&criteria.ref={3}&criteria.states=1" -f $orgName, $project, $repositoryId, $prSourceBranch, $alertType
-
-Write-Host "Will check to see if there are any new $alertType alerts in this PR branch" 
+Write-Host "Will check to see if there are any new CodeQL issues in this PR branch" 
 Write-Host "PR source : $($prSourceBranch). PR target: $($prTargetBranch)"
 
 if (${env:BUILD_REASON} -ne 'PullRequest'){
-   Write-Host "This is not a PR into main so all is ok"
+   Write-Host "This build is not part of a Pull Request so all is ok"
    exit 0
 }
 
@@ -31,12 +29,12 @@ $alertsPRTarget = Invoke-WebRequest -Uri $urlTargetAlerts -Headers $headers -Met
 $alertsPRSource = Invoke-WebRequest -Uri $urlSourceAlerts -Headers $headers -Method Get
 
 if ($alertsPRTarget.StatusCode -ne 200){
-   Write-Host "##vso[task.logissue type=error] Error getting alerts from Azure DevOps Advanced Security Main:", $alertsPRTarget.StatusCode, $alertsPRTarget.StatusDescription
+   Write-Host "##vso[task.logissue type=error] Error getting alerts from Azure DevOps Advanced Security PR target branch:", $alertsPRTarget.StatusCode, $alertsPRTarget.StatusDescription
    exit 1
 }
 
 if ($alertsPRSource.StatusCode -ne 200){
-   Write-Host "##vso[task.logissue type=error] Error getting alerts from Azure DevOps Advanced Security PR:", $alertsPRSource.StatusCode, $alertsPRSource.StatusDescription
+   Write-Host "##vso[task.logissue type=error] Error getting alerts from Azure DevOps Advanced Security PR source branch:", $alertsPRSource.StatusCode, $alertsPRSource.StatusDescription
    exit 1
 }
 
@@ -52,7 +50,7 @@ $newAlertIds = Compare-Object $prSourceAlertIds $prTargetAlertIds -PassThru | Wh
 
 # Are there any new alert ids in the PR source branch?
 if($newAlertIds.length -gt 0) {
-    Write-Host "##[error] There are more alerts in the PR source branch $($prSourceBranch), compared to target branch $($prTargetBranch) :"
+    Write-Host "##[error] The code changes in this PR looks to be introducing new CodeQL alerts:"
 
     # Loop over the objects in the prAlerts JSON object
     foreach ($prAlert in $jsonPRSource.value) {
@@ -73,4 +71,3 @@ if($newAlertIds.length -gt 0) {
     Write-Output "No new CodeQL alerts - all is fine"
     exit 0
 }
-
